65929149a1ecaa5b4a615cd026d918e81be0f0f2
Max <max>
2020-07-13 15:11:14 +0300

    fixed: port on client side

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,7 @@ import sagas from "./sagas/saga";
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(sagaMiddleware)
 ))
 sagaMiddleware.run(sagas)
 
